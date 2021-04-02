9dcacffb7e387678a48351b7310ac747821595fd
ronchitech <ronchibusiness@gmail.com>
2021-04-02 16:42:03 +0800

    Color Context

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import './App.css';
 import UserCreate from './components/UserCreate';
 import LanguageContext from './context/LanguageContext';
+import ColorContext from './context/ColorContext';
 class App extends React.Component {
-  state = { language: 'english' };
+  state = { language: 'english', color: 'red' };
   render() {
     return (
       <div className="ui container">
@@ -11,15 +12,19 @@ class App extends React.Component {
           Select a language:
           <i
             className="flag us"
-            onClick={() => this.setState({ language: 'english' })}
+            onClick={() => this.setState({ language: 'english', color: 'red' })}
           />
           <i
             className="flag nl"
-            onClick={() => this.setState({ language: 'dutch' })}
+            onClick={() =>
+              this.setState({ language: 'dutch', color: 'primary' })
+            }
           />
           {this.state.language}
-          <LanguageContext.Provider value={this.state.language} >
-            <UserCreate />
+          <LanguageContext.Provider value={this.state.language}>
+            <ColorContext.Provider value={this.state.color}>
+              <UserCreate />
+            </ColorContext.Provider>
           </LanguageContext.Provider>
         </div>
       </div>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import LanguageContext from '../context/LanguageContext';
+import ColorContext from '../context/ColorContext';
 class Button extends React.Component {
   static contextType = LanguageContext;
   render() {
     // const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
     return (
-      <button className="ui primary button">
-        <LanguageContext.Consumer>
-          {(value) => (value === 'english' ? 'Submit' : 'Voorlegen')}
-        </LanguageContext.Consumer>
-      </button>
+      <ColorContext.Consumer>
+        {(color) => (
+          <button
+            className={`ui button ${color === 'red' ? 'red' : 'primary'}`}
+          >
+            <LanguageContext.Consumer>
+              {(value) => (value === 'english' ? 'Submit' : 'Voorlegen')}
+            </LanguageContext.Consumer>
+          </button>
+        )}
+      </ColorContext.Consumer>
     );
   }
 }
